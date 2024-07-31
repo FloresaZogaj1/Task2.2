6cac267d9d692ffe0ffe3c3308d5bb734264cb92
Floresa Zogaj <floresaa.zogaj@gmail.com>
2024-07-31 07:20:16 +0200

    task2.2

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ToggleContent from './ToggleContent';
+
+function App() {
+  return (
+    <div className="App">
+      <ToggleContent />
+    </div>
+  );
+}
+export default App;
