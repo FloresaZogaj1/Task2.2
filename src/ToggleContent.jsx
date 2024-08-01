a2b2aa0f811be8b947b138588fa17d27725ba2ea
Floresa Zogaj <floresaa.zogaj@gmail.com>
2024-08-01 09:01:43 +0200

    Asignment2.02

--- src/ToggleContent.jsx
+++ src/ToggleContent.jsx
@@ -7,22 +7,18 @@ function ToggleContent() {
   const toggleContent = () => {
     setIsOpen(!isOpen);
   };
+
   return (
-    <div>
+    <div className="toggle-container">
       <div className="toggle-header" onClick={toggleContent}>
         <h2>Why park a domain name in Parkname ?</h2>
-        <span>{isOpen ? '▲' : '▼'}</span>
+        <span>{isOpen ? '▼' : '▲'}</span>
       </div>
       <div className={`toggle-content ${isOpen ? 'open' : ''}`}>
-        {isOpen && (
-          <>
-            <h2>Why park a domain name in Parkname ?</h2>
-            <p>Parkname is the leading industry standard for domain name parking and monetization services. We offer a wide variety of services to help you achieve success.</p>
-            <span>▼</span>
-          </>
-        )}
+        <p>Parkname is the leading industry standard for domain name parking and monetization services. We offer a wide variety of services to help you achieve success.</p>
       </div>
     </div>
   );
 }
+
 export default ToggleContent;
